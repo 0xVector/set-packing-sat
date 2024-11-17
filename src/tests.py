4fb58d8e42cffbf53b4e525c6da6dc51c69af41d
0xVector <hello@0xvector.me>
2024-11-17 15:24:04 +0100

    Add new test generator

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, shuffle
 
 def chance(val: int) -> bool:
     return randint(1, 100) <= val
@@ -6,3 +6,13 @@ def chance(val: int) -> bool:
 def random_problem(n: int, k: int) -> list[set[int]]:
     u = list(range(n))
     return list(filter(lambda x: len(x) > 0, (set(i for i in u if chance(20)) for _ in range(k))))
+
+def random_problem_sparse(n: int, k: int) -> list[set[int]]:
+    SIZE = 4
+    u = list(range(n))
+    problem = set()
+    while len(problem) < k:
+        size = randint(1, SIZE)
+        shuffle(u)
+        problem.add(frozenset(u[:size]))
+    return [set(s) for s in problem]
